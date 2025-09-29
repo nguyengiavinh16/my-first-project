e757cd04a1af899f8063dbf886357718006dc2b4
nguyengiavinh16 <Vinh.NG2416766@sis.hust.edu.vn>
2025-09-29 10:52:35 +0700

    OOP
    heheheee

---
+++ Syntax OOP.py
@@ -0,0 +1,31 @@
+class Car():
+    def __init__(self, color, engine):
+        self.num = 4
+        self.__engine = engine #private
+        self.color = color
+
+    def run(self):
+        print('Beep')
+
+    def get_info_engine(self):
+        print(self.__engine) #Phải có hàm can thiệp từ bên trong
+    
+    #Hàm cũng có thể private và cũng có thể can thiệp từ bên trong
+
+#Tính kế thừa
+class Mercedes(Car):
+    def __init__(self, color, engine, have_AI):
+        super().__init__(color, engine) #Kế thừa color, engine từ Car
+        self.have_AI = have_AI #Tạo thêm thuộc tính
+
+    def run(self):
+        print('Pro pro pro') #Đè phương thức
+
+car1 = Car('red', 'v12')
+car1.run()
+print(car1.num)
+car1.get_info_engine()
+
+Mẹc = Mercedes('Trắng', 'v8', True)
+print(Mẹc.num)
+Mẹc.run()
